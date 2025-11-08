0df1c93dbb8f88e7f3213270332775a84d8121ca
Turbobot <turbobot@vercel.com>
2025-11-08 19:04:16 +0200

    feat(create-turbo): apply official-starter transform

--- src/modules/FixedHeader/data.ts
+++
@@ -1,9 +0,0 @@
-export interface MenuItem {
-    name: string;
-    link: string;
-}
-export const menu: MenuItem[] = [
-    { name: "Home", link: "section1" },
-    { name: "All Crypto", link: "section2" },
-    { name: "Crypto Wallet", link: "section3" },
-];
